d662251d518dd89c11044046a783c851c34c36d5
s9r-5 <81210115+s9r-5@users.noreply.github.com>
2023-02-07 14:34:54 +0530

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 
 public class Hello {
   
-  public static String GREETING = "Hello world! Hello HelloHeHello world!llo world! world!world! Hello world!";
+  public static String GREETING = "Hello world! Hello  world!llo world! world!world! Hello world!";
   
   public static void main(String []args) {
     
